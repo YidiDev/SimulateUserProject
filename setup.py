fc175fe49dabd23d37dab3262e0db51af7219de7
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-09 23:30:41 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-simulate-user',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['simulateUserProject']),
     include_package_data=True,
     license='MIT License',
