78772bbc5965bfc55478b602bee6519dea3aa43e
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-09 23:55:55 -0400

    releasing 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-simulate-user',
-    version='0.3',
+    version='1.0',
     packages=find_packages(exclude=['simulateUserProject']),
     include_package_data=True,
     license='MIT License',
