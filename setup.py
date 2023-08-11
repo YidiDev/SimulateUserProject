a030c239ec4d04e55b9da29c4a680d4502606301
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-10 20:42:55 -0400

    creating a new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-simulate-user',
-    version='1.0',
+    version='1.001',
     packages=find_packages(exclude=['simulateUserProject']),
     include_package_data=True,
     license='MIT License',
